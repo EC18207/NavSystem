ee393fc9fdfd56fba8c74b502c3d565a503c6157
harrisap <harrisap@rose-hulman.edu>
2019-11-05 20:38:38 -0500

    Added File IO stuff and started reader

--- src/FileReader.java
+++ src/FileReader.java
@@ -1,4 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class FileReader {
-
+	
+	public void fileReadIn(File textFile) {
+			Scanner s = null;
+		try {
+			s = new Scanner(textFile);
+		} catch (FileNotFoundException e) {
+			System.out.println("Could not find file!");
+			e.printStackTrace();
+		}
+		
+		int lineNumber = 1;
+		while(s.hasNext()) {
+			String line = s.nextLine();
+			System.out.println("Line " + lineNumber + ": " + line);
+			lineNumber++;
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
 }
