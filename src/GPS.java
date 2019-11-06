3a6bc9aca6946e6320e9615d87a7f1661de478cc
kriscija <kriscija@kriscija-5520.rose-hulman.edu>
2019-11-06 11:05:48 -0500

    uhh lol

--- src/GPS.java
+++ src/GPS.java
@@ -1,16 +1,12 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class GPS {
 
 	HashMap<String, Point> map;
-	ArrayList<Point> points;
 	
 	public GPS() {
 		NodeInserter inserter = new NodeInserter();
 		map = inserter.completeMap();
-		points = inserter.getImportantPoints();
-		
 		GUIrough gui = new GUIrough();
 	}
 	
