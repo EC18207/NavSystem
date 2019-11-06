2f343987c9408b6f068c8920d93453aa647de71d
hughesm1 <hughesm1@rose-hulman.edu>
2019-11-05 20:08:09 -0500

    added image of map

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		GUIrough todisp = new GUIrough();
 		Point2D.Double in1 = new Point2D.Double(56.00, 37.00);
 		Point2D.Double in2 = new Point2D.Double(156.00, 136.00);
-		todisp.update(in1, in2);
+		todisp.update(in1, in2); 
 	}
 
 }
