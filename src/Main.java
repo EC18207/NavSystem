c7a074a6e1059f4289649a2561796a291a94acd5
harrisap <harrisap@rose-hulman.edu>
2019-11-13 19:49:21 -0500

    beia

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		XMLCache cache = new XMLCache();
+		cache.clearCache();
 		System.out.println("Started Program.");
 		
 		GUI gui = new GUI();
