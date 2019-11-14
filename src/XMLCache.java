cd863ff63c9c228e8abcbcce2cb17d7a6a67c351
harrisap <harrisap@rose-hulman.edu>
2019-11-13 19:24:00 -0500

    uytfghujiuytrfgyhutr

--- src/XMLCache.java
+++ src/XMLCache.java
@@ -108,57 +108,5 @@ public class XMLCache {
 		
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-public class XMLPath implements Serializable {
-	
-	private Point from;
-	private Point to;
-	private ArrayList<Point> path;
-	
-	public XMLPath() {
-	}
-	
-//		public MyPath(Point from, Point to, ArrayList<Point> path) {
-//			
-//			this.from = from;
-//			this.to = to;
-//			this.path = path;
-//			
-//		}
-
-	public Point getFrom() {
-		return from;
-	}
-
-	public void setFrom(Point from) {
-		this.from = from;
-	}
-
-	public Point getTo() {
-		return to;
-	}
-
-	public void setTo(Point to) {
-		this.to = to;
-	}
-
-	public ArrayList<Point> getPath() {
-		return path;
-	}
-
-	public void setPath(ArrayList<Point> path) {
-		this.path = path;
-	}
-
-}
-	
 }
 
