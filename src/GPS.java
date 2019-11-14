7bf36462a3a26c9f8c387d2ef4efc0471e742497
harrisap <harrisap@rose-hulman.edu>
2019-11-13 23:31:29 -0500

    fglhghj

--- src/GPS.java
+++ src/GPS.java
@@ -321,4 +321,35 @@ public class PriorityQueue extends ArrayList<Node> implements Serializable {
 }
 
 
+/* Creates a node used for A* calculations*/
+public class Node {
+
+	Point point = null;
+	double g = 0;
+	double h = -1;
+	double f = -1;
+	Point prevNode = null;
+	
+	public Node(Point physicalNode) {
+		this.point = physicalNode;
+	} 
+
+	public void setG(double calcedG) {
+		this.g = calcedG;
+	}
+	public void calcH(Point p) {
+		double x = p.getX() - this.point.getX();
+		double y = p.getY() - this.point.getY();
+		this.h = Math.sqrt((x*x)+(y*y));
+	}
+	public void calcF() {
+		this.f = 10*(this.g + this.h);
+	}
+	public void setPrevNode(Point name) {
+		this.prevNode = name;
+	}
+	
+}
+
+
 }
