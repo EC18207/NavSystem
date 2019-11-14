21b64268b6f6ef036aff8f5a9dffa5717eb825a0
harrisap <harrisap@rose-hulman.edu>
2019-11-13 19:58:23 -0500

    cleared cache

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
-		XMLCache cache = new XMLCache();
-		cache.clearCache();
+//		XMLCache cache = new XMLCache();
+//		cache.clearCache();
 		System.out.println("Started Program.");
 		
 		GUI gui = new GUI();
